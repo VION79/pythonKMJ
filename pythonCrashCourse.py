c8e6eac01a262f795700d0c1e2cac965de545f9f
VION79 <vion79@gmail.com>
2023-05-25 13:54:28 -0400

    customFunctionsPart2

--- pythonCrashCourse.py
+++ pythonCrashCourse.py
@@ -5,6 +5,41 @@ def convert_to_fahrenheit(celsius_temp):
     return product + 32
 
 convert_to_fahrenheit(0)
+32.0
 
 convert_to_fahrenheit(14)
+57.2
 
+convert_to_fahrenheit(celsius_temp = 24)
+75.2
+
+def convert_to_fahrenheit(celsius_temp = 0):
+    product = celsius_temp * 1.8
+    return product + 32
+
+convert_to_fahrenheit()
+32.0
+
+##############################    
+
+# Define a easy_money function that accepts no parameters 
+# and always returns the value 100.
+def easy_money ():
+    return 100
+
+
+# Define a best_food_ever function that accepts 
+# no parameters and always returns the string “Sushi”.
+def best_food_ever ():
+    return "Sushi"
+
+
+# Define a convert_to_currency function that accepts a single parameter (an integer). 
+# The function should convert the argument to a string, prefix it with a dollar sign, and return the result.
+# 
+# EXAMPLES:
+# convert_to_currency(15)    =&gt; "$15"
+# convert_to_currency(8)     =&gt; "$8"
+
+def convert_to_currency (integer):
+    return "$" + str(integer)
