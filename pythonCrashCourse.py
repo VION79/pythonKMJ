5cd1c72d05e48f97de2ebd54d2a13696459f4c1c
VION79 <vion79@gmail.com>
2023-05-25 13:37:59 -0400

    customFunctions

---
+++ pythonCrashCourse.py
@@ -0,0 +1,10 @@
+# Declare a function that accepts a temprature in Celsius and returns it in Fahrenheit
+
+def convert_to_fahrenheit(celsius_temp):
+    product = celsius_temp * 1.8
+    return product + 32
+
+convert_to_fahrenheit(0)
+
+convert_to_fahrenheit(14)
+
